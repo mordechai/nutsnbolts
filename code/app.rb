ac410c73bfebdbb191e079db03c8b1bce2f361de
Mordechai <mr.happy.snappy@gmail.com>
2013-05-06 12:32:47 +0300

    Update app.rb

--- code/app.rb
+++ code/app.rb
@@ -42,7 +42,6 @@ def create_connection(az_id)
        :hp_tenant_id =>   ENV['HP_TENANT_ID'],
        :hp_avl_zone => az_id,
        :hp_use_upass_auth_style => "true",
-       :user_agent => "console_mordechai/15.185.230.23",
        :connection_options => { :connect_timeout => 30, :read_timeout => 30, :write_timeout => 30 })
 
 end
